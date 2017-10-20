8a6ea46d12c93dd6568c15bff0469ca0ac3f8715
goudan <dsfds@jj.com>
2017-10-20 10:51:54 +0800

    123

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,17 +1,11 @@
 <?php
 namespace app\index\controller;
-
-use think\Db;
-use app\index\model\Admin;
-class Index
+use think\Controller;
+class Index extends Controller
 {
     public function index()
     {
-    /* $data = ['admin_name'=>'sjj','password'=>md5('123456'),'email'=>'513664254@qq.com','phone'=>'18826133237'];
-      //Db::name('admin')->insert($data);
-      $admin = new Admin();
-      $admin->data($data);
-      $admin->save();*/
 
+        return $this->fetch();
     }
 }
